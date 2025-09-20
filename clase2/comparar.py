3c681df775e779293222c2e9898b7373fc777f7d
AlanChamorro <alanchamorro.com>
2025-09-20 11:15:28 -0300

    cambios clase 2

---
+++ clase2/comparar.py
@@ -0,0 +1,8 @@
+# operadores de comparacion
+a=10 == 10 #igualdad
+print("a:", a )
+a=10 != 10
+print("a:",a)
+a= 5 > 19
+print("a:", a)
+
---
+++ clase2/variables.py
@@ -0,0 +1,20 @@
+print("Hola Mundo")
+x = 10
+x = "Hola"
+x = 3.14
+x = True
+print
+
+
+
+numero1=10
+numero2=20
+print("suma:" ,numero1+numero2)
+
+dato1= ("hola")
+print(dato1)
+print(type(dato1))
+dato1=100
+print(dato1)
+print(type(dato1))
+print("aklsjdkjf")
