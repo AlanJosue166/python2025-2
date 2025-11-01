418aadd2a6b55e42af23f82c0a48951eb47fc414
AlanChamorro <alanchamorro.com>
2025-11-01 13:32:39 -0300

    clase 7

--- AlanChamorro_2024102013/app.py
+++ AlanChamorro_2024102013/app.py
@@ -1,14 +1,11 @@
-#
 
 from flask import Flask
-from login import login #importamos el bleprint desde login.py
-from logout import logout
+from login import login 
 
 app=Flask(__name__)
-app.register_blueprint(logout)
 app.register_blueprint(login) #registramos el blueprint
 @app.route('/')
 def home():
     return ""
 if __name__ == '__main__':
-    app.run(debug=True,port=5003)
+    app.run(debug=True,port=5003,host='localhost')
